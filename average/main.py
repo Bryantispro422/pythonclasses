c0f4cc3691ddec5e1775c28db1c19b9e52a469b1
Bryantispro422 <cnbeetseh@gmail.com>
2024-12-13 20:29:25 +0100

    secondcommit

---
+++ average/main.py
@@ -0,0 +1,9 @@
+math=57
+english=47
+leadership=56
+history=41
+
+sum=math+english+leadership+history
+print("the sum of all subjects is",sum)
+average=sum/4
+print("the average of all subjects is",average)
